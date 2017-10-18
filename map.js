25bce23823f871f287ab259540b7bb4b7371fd10
Jakamawatan <31306887+Jakamawatan@users.noreply.github.com>
2017-10-18 20:46:13 +1030

    Add files via upload

---
+++ map.js
@@ -0,0 +1,36 @@
+function initMap() {
+    var adelaide = {lat: -34.9285, lng: 138.6007};
+    var infowindow = new google.maps.InfoWindow(); 
+    var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 14,
+    center: adelaide
+    });
+    
+    map.data.loadGeoJson('map.json');
+    
+    // When the user clicks, open an infowindow
+    map.data.addListener('click', function(event) {
+	var desc = '<h1>' + event.feature.getProperty("description");
+        
+    infowindow.setContent("<div style='width:570px;'>"+desc+"</div>"+
+      '</div>');
+	// position the infowindow on the marker
+	infowindow.setPosition(event.feature.getGeometry().get());
+	// anchor the infowindow on the marker
+	infowindow.setOptions({pixelOffset: new google.maps.Size(0,-30)});
+	infowindow.open(map);
+    });
+
+    map.data.addListener('mouseover',function(event){
+    var name = event.feature.getProperty("name");
+    infowindow.setContent("<div>"+name+"</div>"+
+      '</div>');
+	// position the infowindow on the marker
+	infowindow.setPosition(event.feature.getGeometry().get());
+	// anchor the infowindow on the marker
+	infowindow.setOptions({pixelOffset: new google.maps.Size(0,-30)});
+	infowindow.open(map);
+    });
+}
+ google.maps.event.addDomListener(window, 'load', initialize);
+    
